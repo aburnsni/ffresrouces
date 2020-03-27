7df71904666f918980ef85d27cf4e63e0429ae37
Andrew Burns <aburnsni@gmail.com>
2020-03-27 16:35:35 +0000

    Themeing of search results - still needs work.

--- template.php
+++ template.php
@@ -1,8 +1,13 @@
 <?php
 
 /**
- * @file
- * Bootstrap sub-theme.
- *
- * Place your custom PHP code in this file.
+ * Implements template_preprocess_search_result
+ * @param type $vars
  */
+/** function ffresources_preprocess_search_result(&$vars) {
+  $node = $vars['result']['node'];
+  if ($node->nid) { // if the result is a node we can load the teaser
+    $vars['teaser'] = node_view($node, 'teaser'); 
+  }
+}
+*/
